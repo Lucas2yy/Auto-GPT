a8e63dc3c457e6b6de8d8a64602b2823043bce4a
Lucas2yy <130980035+Lucas2yy@users.noreply.github.com>
2023-04-17 07:21:06 -0300

    Update and rename tests/context.py to testes/context.py

---
+++ testes/context.py
@@ -0,0 +1,8 @@
+import os
+ sistema de importação
+
+. _    caminho . insira (integrar 
+    0 , os . caminho . abspath ( os . path . join ( os . path . dirname ( __file__ ), "../scripts" ) na  6lworkan
+                                a
+                                
+)
